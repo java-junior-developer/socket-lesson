e46fafa2f855c7976a44c84bcf282b674f65296c
dasha <dasha@gmail.com>
2023-12-01 23:35:18 +0300

    Client, Server

--- client/src/study/socket/client/ClientApplication.java
+++ client/src/study/socket/client/ClientApplication.java
@@ -1,8 +1,14 @@
 package study.socket.client;
 
 
+import java.net.InetSocketAddress;
+
 public class ClientApplication {
     public static void main(String[] args) {
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 2222);
+        new Client(address).run();
 
+        // Client client = new Client(address);
+        // client.run();
     }
 }
--- server/src/study/soket/server/Server.java
+++ server/src/study/soket/server/Server.java
@@ -1,9 +1,38 @@
 package study.soket.server;
 
+import study.socket.common.ConnectionService;
+import study.socket.common.Message;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server implements Runnable{
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
 
     @Override
     public void run() {
-
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Сервер запущен");
+            while (true) {
+                // throws IOException
+                try (ConnectionService service = new ConnectionService(serverSocket.accept())){
+                    Message message = service.readMessage();
+                    System.out.println(message.getText());
+                    service.writeMessage(new Message("from server"));
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Ошибка подключение клиента");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            // скорее всего порт уже занят
+            System.out.println("Ошибка создания serverSocket.");
+        }
     }
 }
