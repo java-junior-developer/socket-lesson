f2104fd9aa734855392b39d77522de9f8bf83ea9
jokend1ng <75688359+jokend1ng@users.noreply.github.com>
2023-12-11 10:46:54 +0300

    Update Server.java

--- server/src/study/soket/server/Server.java
+++ server/src/study/soket/server/Server.java
@@ -4,9 +4,7 @@ import study.socket.common.ConnectionService;
 import study.socket.common.Message;
 import study.socket.common.commands.AllComands;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -16,47 +14,69 @@ import java.util.concurrent.TimeUnit;
 public class Server implements Runnable {
     int port;
     MyExecutorService myExecutorService;
+    CopyOnWriteArraySet copyOnWriteArraySet;
 
     public Server(int port) {
         this.port = port;
-        this.myExecutorService = new MyExecutorService (1,8,100,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<>(100));
+        this.myExecutorService = new MyExecutorService(0, 8, 100, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(100));
+        this.copyOnWriteArraySet =new CopyOnWriteArraySet<>();
     }
 
     AllComands commands = new AllComands();
+
     @Override
     public void run() {
         try (ServerSocket SVsocket = new ServerSocket(2222)) {
             System.out.println("Server running");
             while (true) {
                 try {
-
                     Socket socket = SVsocket.accept();
-                    ConnectionService conn = new ConnectionService(socket);
+                    myExecutorService.execute(() -> handler(socket));
 
-                    Message message = conn.readMessage();
-                    if (commands.getCommands().keySet().contains(message.getText())) {
-
-                        commands.getCommands().get(message.getText()).execute();
-
-                    }else{
-
-                    System.out.println(message);
-                    conn.writeMessage(new Message("from server"));}
-                    conn.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println("Соединение сорвалось");
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
 
-
             }
+
         } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Сервер не запустился");
+            throw new RuntimeException(e);
         }
     }
-}
+
+    private void handler(Socket socket) {
+            ConnectionService conn = null;
+            try {
+                conn = new ConnectionService(socket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Message message = null;
+            try {
+                message = conn.readMessage();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            if (commands.getCommands().keySet().contains(message.getText())) {
+
+                commands.getCommands().get(message.getText()).execute();
+
+            } else {
+
+                System.out.println(message.getText());
+                try {
+                    conn.writeMessage(new Message("from server"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            try {
+                conn.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
